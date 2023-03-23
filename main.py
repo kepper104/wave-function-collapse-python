38d9661e63a48d6383a702a9fe66a1d061175f82
kepper104 <67190416+kepper104@users.noreply.github.com>
2023-03-23 22:35:03 +0300

    Fixes

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from pygametest import GridWindow
 from time import perf_counter
 
 COLORS = {
-    4: (50, 50, 50),
-    3: (105, 116, 60),
-    2: (255, 223, 39),
-    1: (39, 120, 255)
+    1: (50, 50, 50),
+    2: (105, 116, 60),
+    3: (255, 223, 39),
+    4: (39, 120, 255)
 }
 class WaveFunctionCollapse:
     def __init__(self, field_size, grid_window=None):
         self.rules = {
-            4: {3, 4},  # mountains
-            3: {4, 3, 2},  # forest
-            2: {3, 2, 1},  # beach
-            1: {1}  # water
+            1: {1, 2},  # mountains
+            2: {1, 2, 3},  # forest
+            3: {3, 4},  # beach
+            4: {4}  # sea
         }
 
         self.window = grid_window
@@ -31,8 +31,13 @@ class WaveFunctionCollapse:
         self.field_size = field_size
         self.to_collapse = list()
         # self.print()
-        self.choose(self.field_size // 2, self.field_size // 2,  [4])
-        # print("------------------------------")
+        default = list()
+        default.append(1)
+        self.choose(self.field_size // 2, self.field_size // 2,  default)
+        print("PUT DEFAULT INTO CENTER -------------------------------------------------")
+        self.print()
+        # print)
+        print("------------------------------")
         # self.print()
         # print("------------------------------")
 
@@ -68,22 +73,23 @@ class WaveFunctionCollapse:
 
                 if type(cur) is int:
                     continue
-
-                if minn[2] > len(cur) > 1:
-                    minn = (x, y, len(cur))
+                is_choosable = len(cur) > 1
+                is_lesser_than_current_min = minn[2] > sum(cur) / 4
+                if is_choosable and is_lesser_than_current_min:
+                    minn = (x, y, sum(cur) / 4)
 
         return minn
 
     def collapse(self, x, y):
-        if len(self.field[x][y]) == 1:
-            self.field[x][y] = self.field[x][y][0]
-        elif len(self.field[x][y]) == 0:
+        if len(self.field[x][y]) == 0:
             print("Error at", x, y)
         else:
             rand_choice = choice(self.field[x][y])
             print(f"Chose {rand_choice} from {self.field[x][y]} at {x}, {y}")
-
-            self.field[x][y] = rand_choice
+            a = list()
+            a.append(rand_choice)
+            self.field[x][y] = a
+            print("UPDATED: ", self.field[x][y])
 
     def propagate(self, x, y):
         self.to_collapse.append((x, y))
@@ -94,12 +100,12 @@ class WaveFunctionCollapse:
 
             if self.window is not None:
                 a = self.field[current[0]][current[1]]
-                if type(a) == int:
-                    # self.window.change_value(current[0], current[1], a)
-                    self.window.cycle_color(current[0], current[1], COLORS[a])
 
-                elif len(a) == 1:
-                    # self.window.change_value(current[0], current[1], a[0])
+                print(a, [current[0]], [current[1]])
+                # self.print()
+                self.window.change_values(current[0], current[1], a)
+
+                if len(a) == 1:
                     self.window.cycle_color(current[0], current[1], COLORS[a[0]])
                 # self.window.external_update()
 
@@ -114,6 +120,8 @@ class WaveFunctionCollapse:
             for i in adjacent_cells:
                 # print(self.field[current[0]][current[1]])
                 # print(list)
+                if i in self.changed:
+                    continue
                 iterate = list()
                 a = self.field[current[0]][current[1]]
                 if type(a) == int:
@@ -143,13 +151,16 @@ class WaveFunctionCollapse:
                         c.add(j)
                 res_options = c.intersection(possible_variants)
                 if len(res_options) == 0:
-                    print("ZERO LIST")
+                    print("ZERO LIST", i)
+                    continue
                     self.print()
                     self.window.external_update()
-                    # input()
-                    res_options.clear()
-                    res_options.add(1)
-                self.field[i[0]][i[1]] = list(res_options)
+                    input()
+                    # res_options.clear()
+                    # res_options.add(1)
+                if len(self.field[i[0]][i[1]]) != 1:
+
+                    self.field[i[0]][i[1]] = list(res_options)
                 if i not in self.to_collapse and i not in self.changed:
                     self.changed.append(i)
                     self.to_collapse.append(i)
@@ -200,7 +211,7 @@ def main():
 
     # WFC.choose(2, 2, 1)
     # WFC.print()
-    print("------------------------")
+    # print("------------------------")
 
     # WFC.propagate(2, 2)
     # WFC.propagate(3, 2)
--- pygametest.py
+++ pygametest.py
@@ -23,7 +23,7 @@ class GridWindow:
         for i in range(self.grid_size):
             row = []
             for j in range(self.grid_size):
-                row.append({'lit': False, 'value': None, 'color': WHITE})
+                row.append({'lit': False, 'values': None, 'color': WHITE})
             self.grid.append(row)
 
     def draw_grid(self):
@@ -32,11 +32,28 @@ class GridWindow:
                 cell_color = self.grid[i][j]['color']
                 cell_rect = pygame.Rect(j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
                 pygame.draw.rect(self.screen, cell_color, cell_rect)
-                if self.grid[i][j]['value'] is not None:
-                    font = pygame.font.SysFont(None, int(self.cell_size * 0.8))
-                    text = font.render(str(self.grid[i][j]['value']), True, (0, 0, 0))
-                    self.screen.blit(text, (j * self.cell_size + self.cell_size // 2 - text.get_width() // 2,
-                                       i * self.cell_size + self.cell_size // 2 - text.get_height() // 2))
+                if self.grid[i][j]['values'] is not None:
+                    font = pygame.font.SysFont(None, int(self.cell_size * 0.5))
+                    values = self.grid[i][j]['values']
+                    if len(values) > 0:
+                        text1 = font.render(str(values[0]), True, (0, 0, 0))
+                        self.screen.blit(text1, (j * self.cell_size + self.cell_size // 2 - text1.get_width() // 2,
+                                       i * self.cell_size + self.cell_size // 2 - text1.get_height() // 2))
+                    if len(values) > 1:
+                        text2 = font.render(str(values[1]), True, (0, 0, 0))
+                        self.screen.blit(text2, (j * self.cell_size + self.cell_size // 2 - text1.get_width() // 2 + 15,
+                                       i * self.cell_size + self.cell_size // 2 - text1.get_height() // 2))
+                    if len(values) > 2:
+                        text3 = font.render(str(values[2]), True, (0, 0, 0))
+                        self.screen.blit(text3, (j * self.cell_size + self.cell_size // 2 - text1.get_width() // 2,
+                                       i * self.cell_size + self.cell_size // 2 - text1.get_height() // 2 + 15))
+                    if len(values) > 3:
+                        text4 = font.render(str(values[3]), True, (0, 0, 0))
+                        self.screen.blit(text4, (j * self.cell_size + self.cell_size // 2 - text1.get_width() // 2 + 15,
+                                       i * self.cell_size + self.cell_size // 2 - text1.get_height() // 2 + 15))
+
+                    # self.screen.blit(text, (j * self.cell_size + self.cell_size // 2 - text.get_width() // 2 + 10,
+                    #                         i * self.cell_size + self.cell_size // 2 - text.get_height() // 2 + 10))
 
     def main_loop(self):
         running = True
@@ -47,7 +64,7 @@ class GridWindow:
                 elif event.type == pygame.KEYDOWN:
                     print("change")
                     self.change_color(2, 2, GRAY)
-                    self.change_value(2, 2, 5)
+                    # self.change_values(2, 2, 5)
                     self.update_screen()
                 elif event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
@@ -72,8 +89,8 @@ class GridWindow:
     def change_color(self, row, col, color):
         self.grid[row][col]['color'] = color
 
-    def change_value(self, row, col, value):
-        self.grid[row][col]['value'] = value
+    def change_values(self, row, col, values):
+        self.grid[row][col]['values'] = values
 
     def cycle_color(self, row, col, color):
         self.grid[row][col]['color'] = color
